ef76c326905524920e2a20a12350d9462a7b1f76
Alessandro D'Orazio <alessandrodorazio@Alessandros-MacBook-Pro.local>
2022-05-03 20:02:14 +0200

    nitro vercel preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     buildModules: ["@nuxtjs/tailwindcss", "vue3-notion/nuxt"],
+    nitro: {
+        preset: 'vercel'
+    }
 })
