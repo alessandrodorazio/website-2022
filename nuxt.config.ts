5e85d49da3b3f79ae63e9dc0cb18ea03550b07ae
Alessandro D'Orazio <alessandrodorazio@Alessandros-MBP.station>
2022-05-04 21:56:58 +0200

    routes test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,39 @@
 import { defineNuxtConfig } from 'nuxt3'
-
+import { resolve } from 'path'
+import { createCommonJS } from 'mlly'
+const { __dirname } = createCommonJS(import.meta.url)
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     buildModules: ["@nuxtjs/tailwindcss", "vue3-notion/nuxt"],
     nitro: {
         preset: 'vercel'
+    },
+    hooks: {
+        'pages:extend' (pages) {
+            pages.push(
+
+                {
+                    name: 'Home',
+                    path: '/',
+                    file: resolve(__dirname, 'pages/index.vue')
+                },
+                {
+                    name: 'Article Page',
+                    path: '/blog:id(.*)',
+                    file: resolve(__dirname, 'pages/blog/[id]/index.vue')
+                },
+
+                {
+                    name: 'List Page',
+                    path: '/demo/:id/:id2',
+                    file: resolve(__dirname, 'pages/List.vue')
+                },
+
+                {
+                    name: 'Not Found',
+                    path: '/:pathMatch(.*)*',
+                    file: resolve(__dirname, 'pages/404.vue')
+                })
+        }
     }
 })
